9eac8ca45df916281e48f07f0be0158550061dba
Sasso <11410120210064@stud.cu.edu.eg>
2023-08-17 01:41:09 +0300

    modify roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) != str:
+    if (type(roman_string) != str) or roman_string is None:
         return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10,
-                  'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    num = 0
-    for i in range(len(roman_string)):
-        if i > 0 and roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
-            num += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i - 1]]
-        else:
-            num += roman_dict[roman_string[i]]
-    return num
+    c, t = 0, 0
+    dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for i in roman_string:
+        if t >= int(dict.get(i)) and t != 0:
+            c += int(dict.get(i))
+        elif t < int(dict.get(i)) and t != 0:
+            c = c + ((int(dict.get(i)) - t) - t)
+        elif t == 0:
+            c += int(dict.get(i))
+        t = int(dict.get(i))
+    return (c)
