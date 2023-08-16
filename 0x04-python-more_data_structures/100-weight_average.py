50bfec1d35f3523bf8f5391169e18a46b0494cda
Sasso <11410120210064@stud.cu.edu.eg>
2023-08-17 01:57:50 +0300

    modify weight avg

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list == []:
+    num, div = 0, 0
+    if my_list:
+        for i in my_list:
+            num += i[0] * i[1]
+            div += i[1]
+        average = num / div
+        return average
+    else:
         return 0
-    sum, div = 0, 0
-    for i in my_list:
-        sum += i[0] * i[1]
-        div += i[1]
-    return sum / div
-
